53fb7a3e2ea73619b47084673a71e2e6caecf5ff
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-05-25 16:57:20 +0800

    adding login feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
     def new
         @user = User.new
     end
     def edit
-        @user = User.find(params[:id])
+       
     end
     def update
-        @user = User.find(params[:id])
+       
         if @user.update(user_params)
             flash[:notice]="Your account information has been successfully updated"
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render 'edit'
         end
@@ -17,6 +18,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if@user.save
+            session[:user_id] = @user.id
             flash[:notice]="Welcome to the Alpha Blog #{@user.username}, you have successfully sign up"
             redirect_to articles_path
         else
@@ -24,8 +26,17 @@ class UsersController < ApplicationController
         end
 
     end
+    def show
+        
+        @articles = @user.articles.paginate(page: params[:page], per_page: 3)
+    end
+    def index
+        @users = User.all
+    end
     private
-    
+    def set_user
+        @user = User.find(params[:id])
+    end
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
