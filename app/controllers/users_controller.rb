d1901122a16db56cb3a3e793922301b5d89d41bb
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-05-29 19:41:06 +0800

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,9 +34,7 @@ class UsersController < ApplicationController
         @users = User.all
     end
     private
-    def set_user
-        @user = User.find(params[:id])
-    end
+     
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
